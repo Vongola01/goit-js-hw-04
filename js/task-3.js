7affda603767b6d6e100aac4a76fbf7249f0e019
Vongola <vongola124@gmail.com>
2024-10-17 00:25:44 +0200

    add task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,18 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    
+    changeUsername(newName) {
+        profile.username = newName;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+    updatePlayTime(newTime) {
+        profile.playTime += newTime;
+    }
+};
+
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
@@ -5,4 +20,4 @@ profile.changeUsername("Marco");
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log(profile.getInfo()); // "Marco has 320 active hours!"{}
